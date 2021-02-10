b89a1c98d2d74b8da6149f50a16dfb735f710409
Lesli Chavez Contreras <lchavezcontreras1@my.wctc.edu>
2021-02-10 15:49:59 -0600

    created my own script

---
+++ script.js
@@ -0,0 +1,20 @@
+const imageDirectory = "cards-png-100x153-numbered";
+//similar to let but cannot be changed
+$(document).ready(function (){
+    $("img#drawDeck").click(drawCard);
+    $("button#discardAllButton").click(discardAllCards);
+});
+function drawCard(){
+    let randomNumber = Math.floor(Math.random()*52)+1;
+    let cardImageTag = $("<img>");
+    let srcAddress = `${imageDirectory}/${randomNumber}`;
+    cardImageTag.attr("src", srcAddress);
+    cardImageTag.attr("alt", "playing card");
+    $("div#playArea").append(cardImageTag);
+
+}
+function discardAllCards(){
+    let lastCardDealt = $("div#playArea img:last-child");
+    $("div#playArea img").remove();
+    $("div#discardPile").append(lastCardDealt);
+}
